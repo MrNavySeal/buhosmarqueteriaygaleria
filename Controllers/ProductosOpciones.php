d0f46fe192f6d4b098aa75f13f451f3778047a79
David <davidstiven1999@hotmail.com>
2024-03-31 23:30:16 -0500

    CRUD variantes producto

--- Controllers/ProductosOpciones.php
+++ Controllers/ProductosOpciones.php
@@ -35,6 +35,18 @@
                 die();
             }
         }
+        public function variantes(){
+            if($_SESSION['permitsModule']['r']){
+                $data['page_tag'] = "variante";
+                $data['page_title'] = "Productos | Variantes de producto";
+                $data['page_name'] = "variantes";
+                $data['panelapp'] = "functions_products_variants.js";
+                $this->views->getView($this,"variantes",$data);
+            }else{
+                header("location: ".base_url());
+                die();
+            }
+        }
         /*************************Measures methods*******************************/
         public function getMeasures(){
             if($_SESSION['permitsModule']['r']){
@@ -241,6 +253,116 @@
                     }else{
                         $id = intval($_POST['id']);
                         $request = $this->model->deleteSpec($id);
+                        if($request=="ok"){
+                            $arrResponse = array("status"=>true,"msg"=>"Se ha eliminado.");
+                        }else{
+                            $arrResponse = array("status"=>false,"msg"=>"No es posible eliminar, intenta de nuevo.");
+                        }
+                    }
+                    echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+                }
+            }
+            die();
+        }
+
+        /*************************Variants methods*******************************/
+        public function getVariants(){
+            if($_SESSION['permitsModule']['r']){
+                $request = $this->model->selectVariants();
+                if(count($request)>0){
+                    for ($i=0; $i < count($request); $i++) { 
+
+                        $btnEdit="";
+                        $btnDelete="";
+                        $status="";
+                        if($request[$i]['status']==1){
+                            $status='<span class="badge me-1 bg-success">Activo</span>';
+                        }else{
+                            $status='<span class="badge me-1 bg-danger">Inactivo</span>';
+                        }
+
+                        if($_SESSION['permitsModule']['u']){
+                            $btnEdit = '<button class="btn btn-success m-1" type="button" title="Editar" onclick="editItem('.$request[$i]['id_variation'].')" ><i class="fas fa-pencil-alt"></i></button>';
+                        }
+                        if($_SESSION['permitsModule']['d']){
+                            $btnDelete = '<button class="btn btn-danger m-1" type="button" title="Eliminar" onclick="deleteItem('.$request[$i]['id_variation'].')"><i class="fas fa-trash-alt"></i></button>';
+                        }
+                        $request[$i]['options'] = $btnEdit.$btnDelete;
+                        $request[$i]['status'] = $status;
+                    }   
+                }
+                echo json_encode($request,JSON_UNESCAPED_UNICODE);
+            }
+            die();
+        }
+        public function getVariant(){
+            if($_SESSION['permitsModule']['r']){
+                if($_POST){
+                    if(empty($_POST)){
+                        $arrResponse = array("status"=>false,"msg"=>"Error de datos");
+                    }else{
+                        $id = intval($_POST['id']);
+                        $request = $this->model->selectVariant($id);
+                        if(!empty($request)){
+                            $arrResponse = array("status"=>true,"data"=>$request);
+                        }else{
+                            $arrResponse = array("status"=>false,"msg"=>"Error, intenta de nuevo"); 
+                        }
+                    }
+                    echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+                }
+            }
+            die();
+        }
+        public function setVariant(){
+            if($_SESSION['permitsModule']['w']){
+                if($_POST){
+                    if(empty($_POST['txtName']) || empty($_POST['options'])){
+                        $arrResponse = array("status" => false, "msg" => 'Error de datos');
+                    }else{ 
+                        $id = intval($_POST['id']);
+                        $arrData = array(
+                            "name"=>ucwords(strClean($_POST['txtName'])),
+                            "options"=>json_decode($_POST['options'],true),
+                            "status"=>intval($_POST['statusList'])
+                        );
+                        if($id == 0){
+                            if($_SESSION['permitsModule']['w']){
+                                $option = 1;
+                                $request= $this->model->insertVariant($arrData);
+                            }
+                        }else{
+                            if($_SESSION['permitsModule']['u']){
+                                $option = 2;
+                                $request = $this->model->updateVariant($id,$arrData);
+                            }
+                        }
+                        if(is_numeric($request) && $request > 0){
+                            if($option == 1){
+                                $arrResponse = array("status"=>true,"msg"=>"Datos guardados.");
+                            }else{
+                                $arrResponse = array("status"=>true,"msg"=>"Datos actualizados.");
+                            }
+                        }else if($request == "exist"){
+                            $arrResponse = array('status' => false, 'msg' => 'La variante ya existe, prueba con otro nombre.');		
+                        }else{
+                            $arrResponse = array("status" => false, "msg" => 'No es posible guardar los datos.');
+                        }
+                    }
+                    echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+                }
+            }
+			die();
+		}
+        public function delVariant(){
+            if($_SESSION['permitsModule']['d']){
+
+                if($_POST){
+                    if(empty($_POST['id'])){
+                        $arrResponse=array("status"=>false,"msg"=>"Error de datos");
+                    }else{
+                        $id = intval($_POST['id']);
+                        $request = $this->model->deleteVariant($id);
                         if($request=="ok"){
                             $arrResponse = array("status"=>true,"msg"=>"Se ha eliminado.");
                         }else{
--- Models/ProductosOpcionesModel.php
+++ Models/ProductosOpcionesModel.php
@@ -2,6 +2,7 @@
     class ProductosOpcionesModel extends Mysql{
         private $intIdMeasure;
         private $intIdSpec;
+        private $intId;
         private $arrData;
         public function __construct(){
             parent::__construct();
@@ -124,6 +125,90 @@
         public function selectSpecs(){
             $sql = "SELECT * FROM specifications";
             $request = $this->select_all($sql);
+            return $request;
+        }
+
+        /*************************Variants methods*******************************/
+        public function insertVariant(array $arrData){
+            $this->arrData = $arrData;
+			$return = 0;
+			$sql = "SELECT * FROM variations WHERE name = '{$this->arrData['name']}'";
+			$request = $this->select_all($sql);
+
+			if(empty($request)){ 
+				$sql  = "INSERT INTO variations(name,status)  VALUES(?,?)";
+	        	$arrData = array(
+                    $this->arrData['name'],
+                    $this->arrData['status']
+                );
+	        	$request = $this->insert($sql,$arrData);
+                $this->insertOptions($request,$this->arrData['options']);
+	        	$return = $request;
+			}else{
+				$return = "exist";
+			}
+	        return $return;
+		}
+        public function insertOptions($id,$data){
+            $total = count($data);
+            $sql = "DELETE FROM variation_options WHERE variation_id = $id; SET @autoid :=0; 
+            UPDATE variation_options SET id_options = @autoid := (@autoid+1);
+            ALTER TABLE variation_options Auto_Increment = 1;";
+            $this->delete($sql);
+            for ($i=0; $i < $total; $i++) { 
+                $sql = "INSERT INTO variation_options(variation_id,name) VALUES (?,?)";
+                $arrData = array($id,ucwords($data[$i]));
+                $this->insert($sql,$arrData);
+            }
+        }
+        public function updateVariant(int $id,array $arrData){
+            $this->intId = $id;
+            $this->arrData = $arrData;
+
+			$sql = "SELECT * FROM variations WHERE name = '{$this->arrData['name']}' AND id_variation != $this->intId";
+			$request = $this->select_all($sql);
+            $return = 0;
+			if(empty($request)){
+                
+                $sql = "UPDATE variations SET name=?,status=? WHERE id_variation = $this->intId";
+                $arrData = array(
+                    $this->arrData['name'],
+                    $this->arrData['status']
+                );
+                $request = $this->update($sql,$arrData);
+                $this->insertOptions($id,$this->arrData['options']);
+	        	$return = intval($request);
+			}else{
+				$return = "exist";
+			}
+			return $return;
+		
+		}
+        public function deleteVariant($id){
+            $this->intId = $id;
+            $sql = "DELETE FROM variations WHERE id_variation = $this->intId";
+            $request = $this->delete($sql);
+            return $request;
+        }
+        public function selectVariant($id){
+            $this->intId = $id;
+            $sql = "SELECT * FROM variations WHERE id_variation = $this->intId";
+            $request = $this->select($sql);
+            $sql="SELECT * FROM variation_options WHERE variation_id = $this->intId";
+            $request['options'] = $this->select_all($sql);
+            return $request;
+        }
+        public function selectVariants(){
+            $sql = "SELECT * FROM variations";
+            $request = $this->select_all($sql);
+            if(!empty($request)){
+                $total = count($request);
+                for ($i=0; $i < $total; $i++) { 
+                    $id = $request[$i]['id_variation'];
+                    $sql ="SELECT count(*) as total FROM variation_options WHERE variation_id = $id";
+                    $request[$i]['qty'] = $this->select($sql)['total']." opciones";
+                }
+            }
             return $request;
         }
         
