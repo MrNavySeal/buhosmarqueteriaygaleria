d5c529d5dd04b1006ffb5017834b191553819a2c
MrNavySeals <dsparrado197023@gmail.com>
2025-06-14 15:10:26 -0500

    dev

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     require_once ("Libraries/Core/Errores.php");
     require_once ("Helpers/Helpers.php");
     require_once("Libraries/Core/Autoload.php");
-    require_once ("Libraries/Core/Router.php");
     require_once ("Libraries/TCPDF/tcpdf.php");
+    require_once ("Libraries/Core/Router.php");
     require_once ("Libraries/vendor/autoload.php");
     require_once ("Libraries/html2pdf/vendor/autoload.php");
 ?>
