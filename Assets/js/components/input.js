7b7f5aff1a6b2f728311f52ca04e524001119a9a
MrNavySeals <dsparrado197023@gmail.com>
2025-07-20 15:27:37 -0500

    dev

--- Assets/js/components/input.js
+++ Assets/js/components/input.js
@@ -1,8 +1,8 @@
 export default {
     template:`
         <div class="mb-3" v-if="type != 'hidden'">
-            <label :for="label" class="form-label">{{label}}</label>
-            <input :type="type" :placeholder="placeholder" :value="modelValue" :required=required :disabled=disabled class="form-control" :id="label" @input="$emit('update:modelValue', $event.target.value)">
+            <label :for="label" class="form-label">{{label}} <span class="text-danger" v-if="required == 'true'">*</span></label>
+            <input :type="type" :placeholder="placeholder" :value="modelValue" :required="required=='true' ? true : false" :disabled=disabled class="form-control" :id="label" @input="$emit('update:modelValue', $event.target.value)">
         </div>
         <input v-else :type="type" :placeholder="placeholder" :value="modelValue" :required=required :disabled=disabled class="form-control" :id="label" @input="$emit('update:modelValue', $event.target.value)">
     `,
