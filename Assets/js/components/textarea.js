843cd34cd21c5f7545a370ba0015bfc5f11bbe9b
MrNavySeals <dsparrado197023@gmail.com>
2025-07-22 20:45:45 -0500

    componentes

---
+++ Assets/js/components/textarea.js
@@ -0,0 +1,18 @@
+export default {
+    template:`
+        <div class="mb-3">
+            <label :for="label" class="form-label">{{label}} <span class="text-danger" v-if="required == 'true'">*</span></label>
+            <textarea  :rows="rows" :placeholder="placeholder" :value="modelValue" :required="required=='true' ? true : false" :disabled=disabled class="form-control" :id="label" @input="$emit('update:modelValue', $event.target.value)"></textarea>
+        </div>
+    `,
+    props:{
+        modelValue:[String,Number],
+        label:"",
+        disabled:"",
+        placeholder:"",
+        required:false,
+        rows:"5"
+    },
+    emits:['update:modelValue'],
+
+}
