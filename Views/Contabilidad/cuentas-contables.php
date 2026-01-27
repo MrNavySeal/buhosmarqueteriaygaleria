9f0915351416a51081a7b56265ec076ebcf24d81
MrNavySeals <dsparrado197023@gmail.com>
2026-01-26 22:05:03 -0500

    asignacion de permisos a cuentas contables

--- Views/Contabilidad/cuentas-contables.php
+++ Views/Contabilidad/cuentas-contables.php
@@ -9,8 +9,12 @@
                     <p class="m-0" :class="clase.status == 2 ? 'text-danger' : ''">{{clase.code}}</p>
                     <p class="m-0" :class="clase.status == 2 ? 'text-danger' : ''">{{clase.name}}</p>
                 </button>
+                <?php if($_SESSION['permitsModule']['w']){ ?>
                 <app-button  icon="new" btn="primary" @click="openModal(clase)"></app-button>
+                <?php } ?>
+                <?php if($_SESSION['permitsModule']['u']){ ?>
                 <app-button  icon="edit" btn="success" @click="openModal(clase,'edit')"></app-button>
+                <?php } ?>
             </div>
         </h2>
         <div :id="'collapseOne'+clase.code" class="accordion-collapse collapse" data-bs-parent="#accordionClase">
@@ -23,9 +27,15 @@
                                     <p class="m-0" :class="grupo.status == 2 ? 'text-danger' : ''">{{grupo.code}}</p>
                                     <p class="m-0" :class="grupo.status == 2 ? 'text-danger' : ''">{{grupo.name}}</p>
                                 </button>
+                                <?php if($_SESSION['permitsModule']['w']){ ?>
                                 <app-button  icon="new" btn="primary" @click="openModal(grupo)"></app-button>
+                                <?php } ?>
+                                <?php if($_SESSION['permitsModule']['u']){ ?>
                                 <app-button  icon="edit" btn="success" @click="openModal(grupo,'edit')"></app-button>
+                                <?php } ?>
+                                <?php if($_SESSION['permitsModule']['d']){ ?>
                                 <app-button  v-if="grupo.children.length == 0" icon="delete" btn="danger" @click="del(grupo)"></app-button>
+                                <?php } ?>
                             </div>
                         </h2>
                         <div :id="'collapseOne'+grupo.code" class="accordion-collapse collapse show" :data-bs-parent="'#accordion'+clase.code">
@@ -38,9 +48,15 @@
                                                     <p class="m-0" :class="cuenta.status == 2 ? 'text-danger' : ''">{{cuenta.code}}</p>
                                                     <p class="m-0" :class="cuenta.status == 2 ? 'text-danger' : ''">{{cuenta.name}}</p>
                                                 </button>
+                                                <?php if($_SESSION['permitsModule']['w']){ ?>
                                                 <app-button  icon="new" btn="primary" @click="openModal(cuenta)"></app-button>
+                                                <?php } ?>
+                                                <?php if($_SESSION['permitsModule']['u']){ ?>
                                                 <app-button  icon="edit" btn="success" @click="openModal(cuenta,'edit')"></app-button>
+                                                <?php } ?>
+                                                <?php if($_SESSION['permitsModule']['d']){ ?>
                                                 <app-button  v-if="cuenta.children.length == 0" icon="delete" btn="danger" @click="del(cuenta)"></app-button>
+                                                <?php } ?>
                                             </div>
                                         </h2>
                                         <div :id="'collapseOne'+cuenta.code" class="accordion-collapse collapse show" :data-bs-parent="'#accordion'+grupo.code">
@@ -53,9 +69,15 @@
                                                                     <p class="m-0" :class="subcuenta.status == 2 ? 'text-danger' : ''">{{subcuenta.code}}</p>
                                                                     <p class="m-0" :class="subcuenta.status == 2 ? 'text-danger' : ''">{{subcuenta.name}}</p>
                                                                 </button>
+                                                                <?php if($_SESSION['permitsModule']['w']){ ?>
                                                                 <app-button  icon="new" btn="primary" @click="openModal(subcuenta)"></app-button>
+                                                                <?php } ?>
+                                                                <?php if($_SESSION['permitsModule']['u']){ ?>
                                                                 <app-button  icon="edit" btn="success" @click="openModal(subcuenta,'edit')"></app-button>
+                                                                <?php } ?>
+                                                                <?php if($_SESSION['permitsModule']['d']){ ?>
                                                                 <app-button  v-if="subcuenta.children.length == 0" icon="delete" btn="danger" @click="del(subcuenta)"></app-button>
+                                                                <?php } ?>
                                                             </div>
                                                         </h2>
                                                         <div :id="'collapseOne'+subcuenta.code" class="accordion-collapse collapse show" :data-bs-parent="'#accordion'+cuenta.code">
@@ -66,8 +88,12 @@
                                                                         <p class="m-0" :class="auxiliar.status == 2 ? 'text-danger' : ''">{{auxiliar.name}}</p>
                                                                     </div>
                                                                     <div class="d-flex gap-2">
+                                                                        <?php if($_SESSION['permitsModule']['u']){ ?>
                                                                         <app-button  icon="edit" btn="success" @click="openModal(auxiliar,'edit')"></app-button>
+                                                                        <?php } ?>
+                                                                        <?php if($_SESSION['permitsModule']['d']){ ?>
                                                                         <app-button  icon="delete" btn="danger" @click="del(auxiliar)"></app-button>
+                                                                        <?php } ?>
                                                                     </div>
                                                                 </div>
                                                             </div>
