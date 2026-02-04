e177375e19ec5a7cfec4e32b11a5b6a7e5ba9b13
MrNavySeals <dsparrado197023@gmail.com>
2026-02-03 21:35:12 -0500

    new: control de transacciones en clase mysql

--- Libraries/Core/Mysql.php
+++ Libraries/Core/Mysql.php
@@ -3,8 +3,8 @@
 	class Mysql extends Conexion
 	{
 		private $conexion;
-		private $strquery;
-		private $arrValues;
+		private $query;
+		private $values;
 
 		function __construct()
 		{
@@ -12,55 +12,94 @@
 			$this->conexion = $this->conexion->conect();
 		}
 
-		//Insertar un registro
-		public function insert(string $query, array $arrValues)
-		{
-			$this->strquery = $query;
-			$this->arrVAlues = $arrValues;
-        	$insert = $this->conexion->prepare($this->strquery);
-        	$resInsert = $insert->execute($this->arrVAlues);
-        	if($resInsert)
-	        {
-	        	$lastInsert = intval($this->conexion->lastInsertId());
-	        }else{
-	        	$lastInsert = 0;
-	        }
-	        return $lastInsert; 
+		public function insert(string $query, array $values){
+			$this->query = $query;
+			$this->values = $values;
+            $insert = 0;
+            try {
+                $insert = $this->conexion->prepare($this->query);
+                $insert->execute($this->values);
+				$id = intval($this->conexion->lastInsertId());
+				if($id == 0){
+					$id = $insert->rowCount();
+				}
+			    return $id;
+            } catch (Exception  $e) {
+                $this->getColumnsError($e);
+            }
+
 		}
 		//Busca un registro
-		public function select(string $query)
-		{
-			$this->strquery = $query;
-        	$result = $this->conexion->prepare($this->strquery);
-			$result->execute();
+		public function select(string $query,$values=[]){
+			$this->query = $query;
+			$this->values = $values;
+        	$result = $this->conexion->prepare($this->query);
+			$result->execute($this->values);
         	$data = $result->fetch(PDO::FETCH_ASSOC);
         	return $data;
 		}
 		//Devuelve todos los registros
-		public function select_all(string $query)
-		{
-			$this->strquery = $query;
-        	$result = $this->conexion->prepare($this->strquery);
-			$result->execute();
+		public function select_all(string $query,$values=[]){
+			$this->query = $query;
+			$this->values = $values;
+        	$result = $this->conexion->prepare($this->query);
+			$result->execute($this->values);
         	$data = $result->fetchall(PDO::FETCH_ASSOC);
         	return $data;
 		}
-		//Actualiza registros
-		public function update(string $query, array $arrValues)
-		{
-			$this->strquery = $query;
-			$this->arrVAlues = $arrValues;
-			$update = $this->conexion->prepare($this->strquery);
-			$resExecute = intval($update->execute($this->arrVAlues));
-	        return $resExecute;
+
+		public function update(string $query, array $values){
+			$this->query = $query;
+			$this->values = $values;
+            $update = 0;
+            try {
+                $update = $this->conexion->prepare($this->query);
+			    return intval($update->execute($this->values));
+            } catch (Exception  $e) {
+                $this->getColumnsError($e);
+            }
 		}
-		//Eliminar un registros
-		public function delete(string $query)
-		{
-			$this->strquery = $query;
-        	$result = $this->conexion->prepare($this->strquery);
-			$del = $result->execute();
-        	return $del;
+
+		public function delete(string $query,$values=[]){
+			$this->query = $query;
+			$this->values = $values;
+			try {
+                $result = $this->conexion->prepare($this->query);
+			    return $result->execute($this->values);
+            } catch (Exception  $e) {
+                $this->getColumnsError($e);
+            }
+		}
+
+		private function getColumnsError(Exception $e){
+            $chars = str_split($this->query);
+            $arrColumns = array_filter($chars,function($e){return $e=="?";});
+            $totalChars = count($chars);
+            $totalColumns = count($arrColumns);
+            $totalValues = count($this->values);
+            $totalDifference = abs($totalColumns-$totalValues);
+            $cont = 0;
+            for ($i=0; $i < $totalChars ; $i++) {
+                if($chars[$i] == "?"){
+                    $value = gettype($this->values[$cont]) == "string" ? "'".$this->values[$cont]."'" : $this->values[$cont];
+                    $chars[$i] = $value;
+                    $cont++;
+                }
+            }
+            $chars = implode($chars);
+            throw new Exception($e->getMessage().": columns = $totalColumns, sent values = $totalValues, difference = $totalDifference, failed query = $chars");
+        }
+
+		public function beginTransaction(){
+			return $this->conexion->beginTransaction();
+		}
+
+		public function commit(){
+			return $this->conexion->commit();
+		}
+
+		public function rollBack(){
+			return $this->conexion->rollBack();
 		}
 	}
 
